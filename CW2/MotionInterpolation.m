ecbb9c12e352a146f2f711f726e13faafc371bbb
Yuqi <8285693+yuqiw18@users.noreply.github.com>
2019-03-04 04:02:13 +0000

    update

--- CW2/MotionInterpolation.m
+++ CW2/MotionInterpolation.m
@@ -1,13 +1,21 @@
+% Motion Interpolation 
+% Algorithm based on this paper:
+% Jiang, Huaizu & Sun, Deqing & Jampani, Varun & Yang, Ming-Hsuan & Learned-Miller, Erik & Kautz, Jan. (2017). 
+% Super SloMo: High Quality Estimation of Multiple Intermediate Frames for Video Interpolation. 
+% Retrieved from https://arxiv.org/pdf/1712.00080.pdf
+% Formula (1)
 function interpolatedFrames = MotionInterpolation(currentFrame, nextFrame, flow, frame)
 
     [width,height,channel] = size(currentFrame);
     interpolatedFrames = zeros(width,height,channel,frame);
     
     for f = 1:frame
-        step = f/(frame+1);
-        currentMotionCompensation = imwarp(currentFrame, step*(-flow));
-        nextMotionCompensation = imwarp(nextFrame, (1-step)*(flow));
-        interpolatedFrame = (1-step)*currentMotionCompensation + (step)*nextMotionCompensation;
+        alpha = f/(frame+1);
+        
+        currentMotionCompensation = imwarp(currentFrame, alpha*(-flow));
+        nextMotionCompensation = imwarp(nextFrame, (1-alpha)*(flow));
+        
+        interpolatedFrame = (1-alpha)*currentMotionCompensation + (alpha)*nextMotionCompensation;
         interpolatedFrames(:,:,:,f) = interpolatedFrame;
     end
 end
--- CW2/main_standalone_advanced_own_data.m
+++ CW2/main_standalone_advanced_own_data.m
@@ -14,9 +14,9 @@ outputPath = 'output';
 %% Initialisation
 % Load Optical Flow
 if (exist('flowsFile','var') == 0)
-    disp("@Loading Optical Flow Data");
+    disp("@Load Optical Flow Data");
     tic  
-        flowsFile = load('flow/myflows.mat');
+        flowsFile = load('flow/flows.mat');
         flowsData = flowsFile.flows_a;
     toc
 else
@@ -25,7 +25,7 @@ end
 
 %Load Image Sequence
 if (exist('imageSequence','var') == 0)
-    disp("@Loading Image Sequence");
+    disp("@Load Image Sequence");
     tic  
         rawImageSequence = load_sequence_color(path, prefix, first, last, digits, suffix);
         imageSequence = imresize(rawImageSequence, 0.3);
@@ -41,75 +41,64 @@ imageCount = last - first + 1;
 %% Basic Section
 % 1. Compute a Distance Matrix that encodes the similarity in appearance
 % between all the frames in the collection.
-if (exist('distanceMatrix','var') == 0)
-    distanceMatrix = ComputeDistanceMatrix(imageSequence);
+if (exist('distanceMatrixAdvanced','var') == 0)
+    distanceMatrixAdvanced = ComputeDistanceMatrixAdvanced(imageSequence, flowsData);
 else
-    disp("@Distance Matrix Already Calculated");
+    disp("@Advanced Distance Matrix Already Calculated");
 end
 
+[w,h] = size(distanceMatrixAdvanced);
+
 % 2. Convert the Distance Matrix into a graph.
-
-
-connectionMatrix = distanceMatrix;
-connectionMatrix(connectionMatrix > mean(connectionMatrix(:))) = 0;
-%connectionMatrix = DistanceMatrixRejection(distanceMatrix);
-
-imshow(connectionMatrix);
+connectionMatrix = DistanceMatrixRejection(distanceMatrixAdvanced);
 sparseDistanceMatrix = sparse(connectionMatrix);
 % graph = biograph(sparseDistanceMatrix,[],'ShowArrows','off','LayoutType','equilibrium');
 % view(graph);
 
+% graph = graphminspantree(sparseDistanceMatrix);
+
 % User input
-% Specify the start frame
 selectedImageIndex = input('Please specify the frame index to start:');
 if (selectedImageIndex < first || selectedImageIndex > last)
     disp('@Invalid Frame: Default - First Frame')
     selectedImageIndex = first+1;
 end
 
-% Define a curve
 imshow(imageSequence(:,:,:,selectedImageIndex)),title('Draw a path with at least 5 points');
-pointCount = 0;
-while(pointCount<5)
-    [pathX, pathY]=getline();
-    pointCount=size(pathX,1);
-    if(pointCount<5)
-        % Ask user to draw again if not enough points are collected
-        imshow(imageSequence(:,:,:,selectedImageIndex)),title('At least 5 points, please draw again');
-    end
-end
+
+% pointCount = 0;
+% 
+% while(pointCount<5)
+%     [pathX, pathY]=getline();
+%     pointCount=size(pathX,1);
+%     if(pointCount<5)
+%         % Ask user to draw again if not enough points are collected
+%         imshow(imageSequence(:,:,:,selectedImageIndex)),title('At least 5 points, please draw again');
+%     end
+% end
+
+pointCount = 6;
+pathX = [274.9535073409462,237.3678629690049,224.8393148450244,227.1884176182708,247.5473083197390,289.8311582381729];
+pathY = [151.9192495921696,154.2683523654159,169.1460032626427,189.5048939641109,206.7316476345840,204.3825448613376];
 
 % 3. For each node in the graph, compute the shortest path.
 [~,paths,~] = graphshortestpath(sparseDistanceMatrix,selectedImageIndex);
-[S, ~] = graphconncomp(sparseDistanceMatrix, 'Weak', true);
 
 % 4. Compute the advected location of point s in every other node, using
 % optical flow.
 
-EstimatedClosestAdvLoc = zeros(pointCount-1,2);
+EstimatedClosestAdvLoc = zeros(pointCount,2);
 EstimatedClosestAdvLoc(1,:)=[pathX(1),pathY(1)];
 
 disp("@Compute Closest Advected Path");
 tic  
-    for i = 1:pointCount-1 
-        
-%         % Swap X, Y 
-%         startPoint = [pathY(i),pathX(i)];   
-%         endPoint = [pathY(i+1),pathX(i+1)];    
-%         [closestIndex, closestX, closestY] = FindShortestAdvectedPath(startPoint, endPoint, paths, flowsData);
-%         EstimatedClosestAdvLoc(i+1,:)= [closestX, closestY];
-        
+    for i = 1:pointCount-1    
         startPoint = [pathX(i),pathY(i)];   
         endPoint = [pathX(i+1),pathY(i+1)]; 
-        
-        advectedPositionValue = ComputeAdvectedPosition(startPoint, paths, flowsData); 
-        
+        advectedPositionValue = ComputeAdvectedPosition(startPoint, paths, flowsData);      
         [closestIndex, closestPoint] = FindClosestPoint(endPoint, advectedPositionValue); 
-        
-        EstimatedClosestAdvLoc(i+1,:)= closestPoint;  
-        
+        EstimatedClosestAdvLoc(i+1,:)= closestPoint;   
         closestPaths{i} = paths{closestIndex};   
-        
         [~,paths,~] = graphshortestpath(sparseDistanceMatrix,closestIndex);
     end
 toc
@@ -125,9 +114,45 @@ hold off;
 
 % 5. Pick the path in Paths whose advected location comes closest to the selected point. 
 % Render this path as the output image sequence for this user-drawn segment.
-outputImageSequence = ConvertPathsToImageSequence(closestPaths, imageSequence);
-implay(outputImageSequence);
+[outputIndex, outputImageSequence] = ConvertPathsToImageSequence(closestPaths, imageSequence);
 
-outputImageSequence = imresize(outputImageSequence, [300 400]);
+%Interpolated frames
+frame = 6;
+outputIndexInterpolated = frame * (length(outputIndex)-1) + length(outputIndex);
+% e.g. F-123456-F-123456-F-123456-F
+% F raw frame + frame * (F-1)
 
-save_sequence_color(outputImageSequence,outputPath,'output_basic_',0,4);
+[height,width,channel,~] = size(outputImageSequence);
+interpolatedImageSequence = zeros(height,width,channel,outputIndexInterpolated);
+
+for i = 1: length(outputIndex)-1
+    
+    currentFrameIndex = outputIndex(i);
+    nextFrameIndex = outputIndex(i+1);
+    
+    currentFrame = imageSequence(:,:,:,currentFrameIndex);
+    nextFrame = imageSequence(:,:,:,nextFrameIndex);
+    
+    % Get the optical flow between the current frame and the next frame
+    % Since the previous step has removed duplicated connecting frames
+    % currentFrameIndex will never be the same as nextFrameIndex
+    if (currentFrameIndex > nextFrameIndex)
+        k = (currentFrameIndex - 1)*(currentFrameIndex - 2)/2 + nextFrameIndex;
+        flow = flowsData(:,:,:,k);
+    else
+        k = (nextFrameIndex - 1)*(nextFrameIndex - 2)/2 + currentFrameIndex;
+        flow = -flowsData(:,:,:,k);
+    end
+    
+    interpolatedImage = MotionInterpolation(currentFrame,nextFrame,flow,frame);
+    interpolatedImageSequence(:,:,:,(i-1)*(frame+1)+1) = currentFrame;
+    for f = 1:frame
+        interpolatedImageSequence(:,:,:,(i-1)*(frame+1)+1+f) = interpolatedImage(:,:,:,f);
+    end    
+end
+interpolatedImageSequence(:,:,:,outputIndexInterpolated) = imageSequence(:,:,:,outputIndex(length(outputIndex)));
+
+implay(interpolatedImageSequence);
+save_sequence_color(interpolatedImageSequence,outputPath,'output_adv_',0,4);
+
+
