e685bcfb44a096475a77cc7ed2261565d0f078a0
Yuqi Wang <8285693+yuqiw18@users.noreply.github.com>
2019-03-11 13:12:45 +0000

    update

--- CP/Part1/Reconstruction.m
+++ CP/Part1/Reconstruction.m
@@ -1,11 +1,11 @@
 %close all;
-clearvars -except uvPatternSequence;
+clearvars -except uvPatternSequence decodedUV;
 
 % Setup & Initialisation
 % Options: cube_T1 monkey_T1 notebook_T1 red_T1 sphere_T1 tablet_T1
 % real_crayon_dalek real_tea 
 % capture
-[path, filename, prefix, first, last, digits, suffix, outputPath, threshold] = LoadImageSequenceProfile('real_crayon_dalek');
+[path, filename, prefix, first, last, digits, suffix, outputPath, threshold] = LoadImageSequenceProfile('cube_T1');
 
 % Load selected image sequence
 if (exist('uvPatternSequence','var') == 0)
@@ -19,19 +19,24 @@ else
 end
 
 %% 1,2. Light Patterns Decoding & Unreliable Pixel Elimination
-decodedUV = DecodeUV(uvPatternSequence, threshold);
+if (exist('decodedUV','var') == 0)
+    decodedUV = DecodeUV(uvPatternSequence, threshold);
+else
+    disp("*UV Already Decoded");
+end
 
-u = decodedUV(:,:,1);
-v = decodedUV(:,:,2);
-figure;
-subplot(1,2,1),imagesc(u),title('U');
-subplot(1,2,2),imagesc(v),title('V');
+% u = decodedUV(:,:,1);
+% v = decodedUV(:,:,2);
+% figure;
+% subplot(1,2,1),imagesc(u),title('U');
+% subplot(1,2,2),imagesc(v),title('V');
 
 %% 3,4 Calibration Matrix Setup & Depth Map Computation
 % Options: provided_synthetic calib_synthetic calib_real calib_capture
-%depthMap = ComputeDepthMap(decodedUV, 'calib_real');
+[depthMap,pointCloud] = ComputeDepthMap(decodedUV, 'provided_synthetic');
+%depthMap = ComputeDepthMap2(decodedUV, 'provided_synthetic');
 
 %% 5. Point Cloud Visualisation
-%SavePLY(depthMap, strcat(outputPath,filename, '.ply'));
-
+SavePLY(pointCloud, strcat(outputPath,filename, '.ply'));
+%SavePLY2(depthMap, strcat(outputPath,filename, '.ply'));
 disp('>>Task Complete')
